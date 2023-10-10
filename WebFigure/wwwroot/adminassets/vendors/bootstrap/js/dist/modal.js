d23c8b4d8e34d9d405c94ee760b90f13a61d3566
Akimone <boycute2002@gmail.com>
2023-10-10 23:25:42 +0700

    last

--- WebFigure/wwwroot/adminassets/vendors/bootstrap/js/dist/modal.js
+++ WebFigure/wwwroot/adminassets/vendors/bootstrap/js/dist/modal.js
@@ -1,8 +1,4 @@
-/*!
-  * Bootstrap modal.js v4.3.1 (https://getbootstrap.com/)
-  * Copyright 2011-2019 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
-  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
-  */
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('jquery'), require('./util.js')) :
   typeof define === 'function' && define.amd ? define(['jquery', './util.js'], factory) :
